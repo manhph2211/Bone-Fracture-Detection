25d28823e0388c71bacf6438809cf6a0593159f1
manhph2211 <maxph22112000@gmail.com>
2021-04-11 23:08:45 +0700

    update

--- src/mura/segmentator/MaskRCNN_segmentator/train_xray_segm.py
+++ src/mura/segmentator/MaskRCNN_segmentator/train_xray_segm.py
@@ -9,19 +9,27 @@ model = dict(
 
 # Modify dataset related settings
 dataset_type = 'COCODataset'
-classes = ('balloon',)
+classes = ('bone',)
 data = dict(
     train=dict(
-        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/train/JPEGImages',
+        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/train',
         classes=classes,
         ann_file='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/annotations/instances_train.json'),
     val=dict(
-        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/val/JPEGImages',
+        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/val',
         classes=classes,
         ann_file='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/annotations/instances_val.json'),
     test=dict(
-        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/test/JPEGImages',
+        img_prefix='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/test',
         classes=classes,
         ann_file='/content/gdrive/MyDrive/Data Augmentation-XrayImg/XRAYBoneDataset/annotations/instances_test.json'))
+
+optimizer = dict(
+  type='SGD',
+  lr = 2e-4,
+  momentum=0.9,
+  weight_decay=0.0001
+)
+runner = dict(type='EpochBasedRunner', max_epochs=1)
 
 load_from = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
