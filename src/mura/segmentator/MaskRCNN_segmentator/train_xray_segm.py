c5bf6fd86e4bfda4da5f9b5452ce07b8ec564047
manhph2211 <maxph22112000@gmail.com>
2021-05-03 18:24:19 +0700

    update large dataset

--- src/mura/segmentator/MaskRCNN_segmentator/train_xray_segm.py
+++ src/mura/segmentator/MaskRCNN_segmentator/train_xray_segm.py
@@ -1,7 +1,8 @@
 
 
 _base_ = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmdetection/configs/mask_rcnn/mask_rcnn_r50_caffe_fpn_mstrain-poly_1x_coco.py'
-
+# deeplabv3
+#_base_  = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmsegmentation/configs/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes.py'
 model = dict(
     roi_head=dict(
         bbox_head=dict(num_classes=1),
@@ -30,6 +31,9 @@ optimizer = dict(
   momentum=0.9,
   weight_decay=0.0001
 )
-runner = dict(type='EpochBasedRunner', max_epochs=1)
+runner = dict(type='EpochBasedRunner', max_epochs=5)
+#load_from = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+load_from = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmdetection/work_dirs/train_xray_segm/latest.pth'
 
-load_from = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmdetection/checkpoints/mask_rcnn_r50_caffe_fpn_mstrain-poly_3x_coco_bbox_mAP-0.408__segm_mAP-0.37_20200504_163245-42aa3d00.pth'
+#deeplabv3
+#load_from = '/content/gdrive/MyDrive/Data Augmentation-XrayImg/mmsegmentation/v0.5/deeplabv3plus/deeplabv3plus_r101-d8_512x1024_40k_cityscapes/deeplabv3plus_r101-d8_512x1024_40k_cityscapes_20200605_094614-3769eecf.pth'
